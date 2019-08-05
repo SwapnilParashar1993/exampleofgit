84aeac072c4cdd68782b315c130ce25f15ade493
Swapnil <beetuswap@gmail.com>
2019-08-05 21:12:10 +0530

    changed by swapnil

---
+++ exampleforgit/src/main/java/exampleforgit/exampleforgit/App.java
@@ -0,0 +1,15 @@
+package exampleforgit.exampleforgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+        System.out.println( "Hello India!" );
+
+    }
+}
